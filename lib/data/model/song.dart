421e0ebbbd2420f5cf8b2a7e91ea36df7deae34d
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-09-08 23:00:47 +0700

    feat: add initial MusicApp and Song model

---
+++ lib/data/model/song.dart
@@ -0,0 +1,37 @@
+class Song {
+  Song({
+    required this.id,
+    required this.album,
+    required this.artist,
+    required this.source,
+    required this.image,
+    required this.duration,
+  });
+
+  factory Song.fromJson(Map<String, dynamic> map) {
+    return Song(
+      id: map['id'],
+      album: map['album'],
+      artist: map['artist'],
+      source: map['source'],
+      image: map['image'],
+      duration: map['duration'],
+    );
+  }
+
+  String id;
+  String album;
+  String artist;
+  String source;
+  String image;
+  int duration;
+
+  //Quy dinh 2 "Song" la giong nhau neu chung co cung id
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Song && runtimeType == other.runtimeType && id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class MusicApp extends StatelessWidget {
+  const MusicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+// Ctrl Alt L to format
+// Ctrl Alt O to optimize imports
+// Ctrl Alt M to extract method
+// Ctrl Alt V to extract variable
+// Ctrl Alt F to extract field
+// Ctrl Alt C to extract class
